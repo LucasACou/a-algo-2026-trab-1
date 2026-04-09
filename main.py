ca1ce6f6efe48088764066cc8bc6c89e035fdc0e
LucasACou <lucascoutinho0710@gmail.com>
2026-04-09 15:40:52 -0300

    resolvendo casos com iframe no html

--- main.py
+++ main.py
@@ -5,24 +5,50 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time 
 
-url = "https://www.uol.com.br/"
-x_path = "//a[@title='Dólar']//span[contains(@class,'exchangeBarHeader__item__value')]"
+# "https://www.uol.com.br/"
+# "//a[@title='Dólar']//span[contains(@class,'exchangeBarHeader__item__value')]"
+
+# testes
+# "https://b3.com.br/"
+# "//*[@id="quotes-container"]/div/span/span"
+
+# == functions ==
+def encontrar_elemento(driver, xpath):
+    # 1. tenta direto
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, xpath))
+        )
+        return element
+    except:
+        pass
+
+    # 2. tenta em iframes
+    iframes = driver.find_elements(By.TAG_NAME, "iframe")
+
+    for iframe in iframes:
+        driver.switch_to.frame(iframe)
+        try:
+            element = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.XPATH, xpath))
+            )
+            return element
+        except:
+            driver.switch_to.default_content()
+
+# == main ==
+url = input("Digite o URL: ")
+xpath = input("Digite o Xpath: ")
 
 service = Service(executable_path="msedgedriver.exe")
 driver = webdriver.Edge(service=service)
 
 driver.get(url)
 
-element = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located(
-        (By.XPATH, x_path)
-    )
-)
+element = encontrar_elemento(driver, xpath)
 
-# depois de visível, pega o texto
 valor = element.text
 print("Cotação capturada:", valor)
-
 
 time.sleep(10)
 
