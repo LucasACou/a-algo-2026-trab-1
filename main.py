3e13108f2d8031f99b972efc63e8af4ab6770968
LucasACou <lucascoutinho0710@gmail.com>
2026-04-10 12:22:16 -0300

    teste de login no email

--- main.py
+++ main.py
@@ -3,16 +3,24 @@ from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.edge.options import Options
+from dotenv import load_dotenv
 
-# "https://www.uol.com.br/"
+import time
+import os
+
+#"https://www.uol.com.br/"
 # "//a[@title='Dólar']//span[contains(@class,'exchangeBarHeader__item__value')]"
 
 # testes
-# "https://b3.com.br/"
-# "//*[@id="quotes-container"]/div/span/span"
+url =  "https://b3.com.br/"
+xpath = '//*[@id="quotes-container"]/div/span/span'
 
-# == functions ==
+
+
+
+# ======= functions =======
 def encontrar_elemento(driver, xpath):
     # 1. tenta direto
     try:
@@ -36,20 +44,39 @@ def encontrar_elemento(driver, xpath):
         except:
             driver.switch_to.default_content()
 
-# == main ==
-url = input("Digite o URL: ")
-xpath = input("Digite o Xpath: ")
+
+
+
+
+# ======= main =======
+load_dotenv()
+gmail = os.getenv("GMAIL")
+senha = os.getenv("SENHA")
+
+#url = input("Digite o URL: ")
+#xpath = input("Digite o Xpath: ")
 
 service = Service(executable_path="msedgedriver.exe")
 driver = webdriver.Edge(service=service)
 
+#url de mineração
 driver.get(url)
+original_tab = driver.current_window_handle
 
+#mineração dos dados
 element = encontrar_elemento(driver, xpath)
-
 valor = element.text
 print("Cotação capturada:", valor)
 
-time.sleep(10)
+
+driver.switch_to.new_window('tab')
+driver.get("https://gmail.com")
+
+input_element = driver.find_element(By.XPATH, '//*[@id="identifierId"]')
+input_element.send_keys(gmail + Keys.ENTER)
+
+
+
+time.sleep(100)
 
 driver.quit()
