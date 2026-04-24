517f699c727d2926b72cc939ca3a116b8e25b44d
LucasACou <lucascoutinho0710@gmail.com>
2026-04-23 23:21:28 -0300

    refatorando codigo, adicionando interface amigavel e gerando documentação com sphinx

---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'a-algo-2026-trab-1'
+copyright = '2026, Lucas A Coutinho'
+author = 'Lucas A Coutinho'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'pt_BR'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,26 @@
+.. a-algo-2026-trab-1 documentation master file, created by
+   sphinx-quickstart on Thu Apr 23 21:59:35 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+a-algo-2026-trab-1 documentation - Sistema de Monitoramento
+=======================
+
+Este sistema permite monitorar valores em páginas web utilizando Selenium
+com uma interface gráfica interativa.
+
+Arquitetura
+-----------
+
+- Interface gráfica (Tkinter)
+- Automação web (Selenium)
+- Execução concorrente (Threads)
+- Sistema de logs integrado
+
+Referência da API
+-----------------
+
+.. automodule:: main
+   :members:
+   :undoc-members:
+   :show-inheritance:
